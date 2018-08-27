8f78b3b0ede7c6d05ee60e00448af0167de79ecb
Ankit Srivastava <signore.ankit@github.com>
2018-08-27 14:06:32 +0530

    Numbers and Simple math operations on them

---
+++ numbers.py
@@ -0,0 +1,25 @@
+"""
+    Integer - Whole numbers, eg: 0, 43,77 etc.
+    Floating point numbers - Numbers with a decimal
+"""
+
+# Addition
+print('Addition', 2+2)
+
+# Subtraction
+print('Subtraction', 3-2)
+
+# Division
+print('Division', 3/4)
+
+# Multiplication
+print('Multiplication', 5*7)
+
+# Modulo or Mod operator - returns the remained after the division
+print('Modulo', 97 % 5)
+
+# Exponent - a**b represents a to the power b
+print('Exponent', 2 ** 4)
+
+# Floor Division - Returns the left side of the decimal or simple integer if no decimal
+print('Floor Division', 9 // 2)
